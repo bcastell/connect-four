4ea9f1da5fc1d3c06ab095182f12346238379833
Brandon Castellanos <bcastellanos87@gmail.com>
2017-05-17 20:24:21 -0700

    checks which player won

--- app/components/App.js
+++ app/components/App.js
@@ -17,6 +17,50 @@ class App extends React.Component {
 		};
 	}
 
+	checkWin(holes, color) {
+		// Horizontal Check
+		for (let j = 0; j <= 35; j += 7) {
+			for (let i = j; i < j + 4; ++i) {
+				if (holes[i] === color && holes[i + 1] === color &&
+					holes[i + 2] === color && holes[i + 3] === color) {
+					return true;
+				}
+			}
+		}
+
+		// Vertical Check
+		for (let j = 0; j <= 6; ++j) {
+			for (let i = j; i <= j + 14; i += 7) {
+				if (holes[i] === color && holes[i + 7] === color &&
+					holes[i + 14] === color && holes[i + 21] === color) {
+					return true;
+				}
+			}
+		}
+
+		// Ascending Diagonal Check
+		for (let j = 21; j <= 35; j += 7) {
+			for (let i = j; i < j + 4; ++i) {
+				if (holes[i] === color && holes[i - 6] === color &&
+					holes[i - 12] === color && holes[i - 18] === color) {
+					return true;
+				}
+			}
+		}
+
+		// Descending Diagonal Check
+		for (let j = 24; j <= 38; j += 7) {
+			for (let i = j; i < j + 4; ++i) {
+				if (holes[i] === color && holes[i - 8] === color &&
+					holes[i - 16] === color && holes[i - 24] === color) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
 	handleClick(i) {
 		const holes = this.state.grid.slice();
 		for (let j = 41 - Math.abs(i - 6); j >= 0; j -= 7) {
@@ -24,6 +68,11 @@ class App extends React.Component {
 				holes[j] = this.state.red ? 'R' : 'Y';
 				break;
 			}
+		}
+
+		const color = this.state.red ? 'R' : 'Y';
+		if (this.checkWin(holes, color)) {
+			window.alert("Winner!");
 		}
 
 		this.setState({
